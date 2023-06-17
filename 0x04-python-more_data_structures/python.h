390729e0fa834a4d5aad22a42e03bd9715ed7425
ivytech11 <ommaratz@gmail.com>
2023-06-16 17:46:50 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/python.h
@@ -0,0 +1,19 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+void print_python_bytes(PyObject *p)
+{
+	long int size;
+	int i;
+	char *trying_str = NULL;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+
